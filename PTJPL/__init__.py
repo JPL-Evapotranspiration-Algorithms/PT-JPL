98c0efc7b91abb8c507c9b3ee54fef747cdbab98
Gregory Halverson <gregory.halverson@gmail.com>
2024-11-21 22:40:53 -0800

    initial release v1.0.1

--- PTJPL/__init__.py
+++ PTJPL/__init__.py
@@ -1,1 +1,9 @@
 from .PTJPL import *
+
+from os.path import join, abspath, dirname
+
+with open(join(abspath(dirname(__file__)), "version.txt")) as f:
+    version = f.read()
+
+__version__ = version
+__author__ = "Gregory H. Halverson"
