853877dfc9de1af6e71217dc95a0a4097290acae
Gregory Halverson <gregory.halverson@gmail.com>
2025-03-12 15:30:16 -0700

    unit tests and continuous integration

---
+++ tests/test_import_dependencies.py
@@ -0,0 +1,17 @@
+import pytest
+
+# List of dependencies
+dependencies = [
+    "ECOv002_CMR",
+    "ECOv002_granules",
+    "geos5fp",
+    "numpy",
+    "pandas",
+    "rasters",
+    "sklearn"
+]
+
+# Generate individual test functions for each dependency
+@pytest.mark.parametrize("dependency", dependencies)
+def test_dependency_import(dependency):
+    __import__(dependency)
