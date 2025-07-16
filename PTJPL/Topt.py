b146641a41fdf785f1eaba0483ba9e034ff6e984
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-07-16 15:51:24 -0700

    extracting Topt and fAPARmax at points

--- PTJPL/Topt.py
+++ PTJPL/Topt.py
@@ -1,12 +1,16 @@
+from typing import Union
 from os.path import join, abspath, dirname
 import numpy as np
 import rasters as rt
 from rasters import Raster, RasterGeometry
+from rasters import MultiPoint
 
-def load_Topt(geometry: RasterGeometry) -> Raster:
+def load_Topt(geometry: Union[RasterGeometry, MultiPoint]) -> Union[Raster, np.ndarray]:
     SCALE_FACTOR = 0.01
     filename = join(abspath(dirname(__file__)), "Topt_mean_CMG_int16.tif")
-    image = rt.clip(rt.Raster.open(filename, geometry=geometry, resampling="cubic") * SCALE_FACTOR, 0, None)
-    image.nodata = np.nan
+    result = rt.clip(rt.Raster.open(filename, geometry=geometry, resampling="cubic") * SCALE_FACTOR, 0, None)
+    
+    if isinstance(result, Raster):
+        result.nodata = np.nan
 
-    return image
+    return result
--- PTJPL/fAPARmax.py
+++ PTJPL/fAPARmax.py
@@ -1,12 +1,15 @@
+from typing import Union
 from os.path import join, abspath, dirname
 import numpy as np
 import rasters as rt
-from rasters import Raster, RasterGeometry
+from rasters import Raster, RasterGeometry, MultiPoint
 
-def load_fAPARmax(geometry: RasterGeometry) -> Raster:
+def load_fAPARmax(geometry: Union[RasterGeometry, MultiPoint]) -> Union[Raster, np.ndarray]:
     SCALE_FACTOR = 0.0001
     filename = join(abspath(dirname(__file__)), "fAPARmax_mean_CMG_int16.tif")
-    image = rt.clip(rt.Raster.open(filename, geometry=geometry, resampling="cubic") * SCALE_FACTOR, 0, None)
-    image.nodata = np.nan
-
-    return image
+    result = rt.clip(rt.Raster.open(filename, geometry=geometry, resampling="cubic") * SCALE_FACTOR, 0, None)
+    
+    if isinstance(result, Raster):
+        result.nodata = np.nan
+    
+    return result
