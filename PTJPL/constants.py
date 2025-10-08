49cfb18730df425aa3f51906e76c17ef605b1373
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-10-08 11:08:02 -0700

    using `priestley-taylor`

--- PTJPL/constants.py
+++ PTJPL/constants.py
@@ -4,10 +4,6 @@
 
 RESAMPLING_METHOD = "cubic"
 
-# Priestley-Taylor coefficient alpha (dimensionless)
-# Typical value for unstressed vegetation
-PT_ALPHA = 1.26
-
 # Beta parameters for soil moisture constraint
 # BETA_KPA: beta in units of kPa
 # BETA_PA: beta in units of Pa (1 kPa = 1000 Pa)
